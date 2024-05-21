3bd02fdaabe59de50980ceace298f66b614b9854
bilal96aslam <58890948+bilal96aslam@users.noreply.github.com>
2024-05-21 22:45:39 +0500

    initial commit

---
+++ app/src/main/java/com/iobotics/pokedexapp/ui/theme/Type.kt
@@ -0,0 +1,31 @@
+package com.iobotics.pokedexapp.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.text.font.Font
+import com.iobotics.pokedexapp.R
+
+
+val Roboto = FontFamily(
+    Font(R.font.roboto_light, FontWeight.Light),
+    Font(R.font.roboto_regular, FontWeight.Normal),
+    Font(R.font.roboto_medium, FontWeight.Medium),
+    Font(R.font.roboto_bold, FontWeight.Bold),
+)
+
+val RobotoCondensed = FontFamily(
+    Font(R.font.roboto_condensed_light, FontWeight.Light),
+    Font(R.font.roboto_condensed_regular, FontWeight.Normal),
+)
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    bodySmall = TextStyle(
+        fontFamily = Roboto,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+)
